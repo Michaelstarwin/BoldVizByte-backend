2429a07cde7e1d7ef77a7b513193bf8f8ad029fe
Michael starwin raj M <148256353+Michaelstarwin@users.noreply.github.com>
2025-06-15 23:06:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,13 @@ app.use('/api/contact', contactRoutes);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => {
-  console.log('MongoDB connected');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}).catch(err => console.error("MongoDB connection error:", err));
+})
+ .then(() => {
+  console.log("MongoDB connected");
+  app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
+});
+ })
+ .catch(err => {
+  console.error("MongoDB connection error:", err);
+ });
